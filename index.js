58a2fe6e7063dbf4d54962c0d205e974bed1efdd
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2021-08-26 18:03:57 +0600

    sadfdsa

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const app = express()
+app.set('view engine','ejs')
+// app.set('views','template/views')
+
 const userRouter = require('./router')
 
 app.use('/user',userRouter);
+app.use(morgan('dev'))
 
 
 app.get('/',(req,res)=>{
-    res.send('<h1>I am Listening</h1>')
+    let post = {
+        title:'Test Title',
+        body:'Test Body',
+        published:true,
+    }
+
+    let posts =[
+        {title:'Title One','author':' Qayum Hasan'},
+        {title:'Title two','author':' Qayum Hasan'},
+        {title:'Title three','author':' Qayum Hasan'},
+        {title:'Title four','author':' Qayum Hasan'}
+    ]
+    res.render('index',{title:'EJS is an Awesome Template Engine fdsafdsafdsa',post,posts});
+})
+
+
+app.get('/about',(req,res)=>{
+    res.render('pages/about')
+})
+app.get('*',(req,res)=>{
+    res.send('<h1>Check</h1>');
 })
 
 
